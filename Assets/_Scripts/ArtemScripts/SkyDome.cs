a943e4801e5f8e5d71e2e25631df77aa5b59f72d
Artem Yakubovich <artempaulavich@gmail.com>
2023-10-27 19:17:29 +0200

    Add sky

---
+++ Assets/_Scripts/ArtemScripts/SkyDome.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class SkyDome : MonoBehaviour, IStartable, IUpdatable
+{
+    [SerializeField] private float _speed;
+    
+    public void OnStart()
+    {
+        ServiceLocator.Instance.Get<GameController>().updatablesHolder.Registration(this);
+    }
+    
+    public void EveryFrameRun()
+    {  
+        transform.Rotate(Vector3.up, _speed * Time.deltaTime);
+    }
+
+    
+}
