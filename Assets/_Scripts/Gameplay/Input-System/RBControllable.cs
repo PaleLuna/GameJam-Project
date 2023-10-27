ecd3bf1b9540ed6fefc4148fd1bc5acab5eb3a1c
PaleLuna <robertmkrtyan334@gmail.com>
2023-10-27 20:00:33 +0300

    Добавление модельки персонажа и настройка замка

--- Assets/_Scripts/Gameplay/Input-System/RBControllable.cs
+++ Assets/_Scripts/Gameplay/Input-System/RBControllable.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class RBControllable : IControllable
 {
+    private const float _angle = 90F;
+    
     private Rigidbody _rigidbody;
     private Vector3 _currentDirection;
 
@@ -15,11 +17,13 @@ public class RBControllable : IControllable
     public void Move(float speed)
     {
         Vector3 velocity = _currentDirection * speed;
-
+        Vector3 rotate = _currentDirection * _angle;
+        
         velocity = Vector3.ClampMagnitude(velocity, speed);
         
         velocity.y = _rigidbody.velocity.y;
         _rigidbody.velocity = velocity;
+        _rigidbody.rotation = Quaternion.Euler(rotate);
     }
 
     public void Interaction()
---
+++ Assets/_Scripts/Gameplay/PlayerLookForward.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class PlayerLookForward
+{
+    private Rigidbody _rigidbody;
+    private Vector3 _currentDirection;
+}
