4a3f0ea2fa2eb8746dcdfa4dfe8481883b7fa2b2
Artem Yakubovich <artempaulavich@gmail.com>
2023-10-28 19:13:11 +0200

    add naveMesh

--- Assets/_Scripts/ArtemScripts/Enemy.cs
+++ Assets/_Scripts/ArtemScripts/Enemy.cs
@@ -1,26 +1,30 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace ArtemYakubovich
 {
     public class Enemy : MonoBehaviour, IStartable, IUpdatable
     {
+        [SerializeField] private NavMeshAgent _navMeshAgent;
         [SerializeField] private int _health;
-        [SerializeField] private float _speed;
         private Transform _target;
 
         public void SetTarget(Transform target)
         {
             _target = target;
+            _navMeshAgent = GetComponent<NavMeshAgent>();
         }
         
         public void OnStart()
         {
             ServiceLocator.Instance.Get<GameController>().updatablesHolder.Registration(this);
+            _navMeshAgent.SetDestination(_target.position);
+            print(_target.position);
         }
     
         public void EveryFrameRun()
         {
-            transform.position = Vector3.MoveTowards(transform.position, _target.position, _speed * Time.deltaTime);
+           
         }
     }
 }
